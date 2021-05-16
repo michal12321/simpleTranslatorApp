a179e46d0e7a7f857d27e53fbaac31fb0c875f69
Michal Sobczyk <michal.sobczyk@aol.com>
2021-05-16 21:16:18 +0200

    Added google API post method, picker component

---
+++ translator/src/assets/languages.js
@@ -0,0 +1,42 @@
+export const languages = [
+  {
+    id: 0,
+    name: 'Croatian',
+    isoCode: 'hr',
+  },
+  {
+    id: 1,
+    name: 'Czech',
+    isoCode: 'cs',
+  },
+  {
+    id: 2,
+    name: 'English',
+    isoCode: 'en',
+  },
+  {
+    id: 3,
+    name: 'German',
+    isoCode: 'de',
+  },
+  {
+    id: 4,
+    name: 'Hindi',
+    isoCode: 'hi',
+  },
+  {
+    id: 5,
+    name: 'Polish',
+    isoCode: 'pl',
+  },
+  {
+    id: 6,
+    name: 'Swedish',
+    isoCode: 'sv',
+  },
+  {
+    id: 7,
+    isoCode: 'zu',
+    name: 'Zulu',
+  },
+];
--- translator/src/components/Translator.js
+++ translator/src/components/Translator.js
@@ -1,48 +1,102 @@
 import React, {useState} from 'react';
-import {TextInput, View, StyleSheet, Text} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import {Image, View, StyleSheet, Text} from 'react-native';
+import axios from 'axios';
 
 import TextBox from './common/TextBox';
 import Header from './common/Header';
 import {Colors} from '../styles';
+import {languages} from '../assets/languages';
+import LangPicker from './common/Picker';
 
+const API_KEY = 'AIzaSyCpSTgDiocRg7X5EBALWXNV8vDjJ2ExSlw';
 const CONFIG = {
   HEADER_TITLE: 'SIMPLE TRANSLATOR APP',
-  PICKER_DESCRIPTION: 'Select language to translate to:',
+  PICKER_DESCRIPTION: 'Select languages:',
   INPUT_PLACEHOLDER: 'Touch to write...',
+  OUTPUT_DESCRIPTION: 'Translated text: ',
+  ARROW_PATH: require('../assets/icons/arrow.png'),
 };
 
 const Translator = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [text, setText] = useState();
   const [translatedText, setTranslatedText] = useState('text will appear here');
+  const [baseLanguage, setBaseLanguage] = useState();
+  const [targetLanguage, setTargetLanguage] = useState();
+
+  const translate = () => {
+    axios
+      .post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: baseLanguage,
+            key: API_KEY,
+            format: 'text',
+          },
+        },
+      )
+      .then(res =>
+        setTranslatedText(res.data.data.translations[0].translatedText),
+      )
+      .catch(err => console.log(err));
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header title={CONFIG.HEADER_TITLE} />
       <Text style={styles.pickerDescription}>{CONFIG.PICKER_DESCRIPTION}</Text>
-      <Picker
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)
-        }>
-        <Picker.Item label="Polish" value="pl" />
-        <Picker.Item label="Deutsch" value="de" />
-        <Picker.Item label="Chinesee" value="ch" />
-      </Picker>
-      <TextBox placeholder={CONFIG.INPUT_PLACEHOLDER} />
+      <View style={styles.pickerContainer}>
+        <LangPicker
+          style={styles.picker}
+          pickerItems={languages}
+          selectedLanguage={baseLanguage}
+          setSelectedLanguage={setBaseLanguage}
+        />
+        <Image style={styles.icon} source={CONFIG.ARROW_PATH} />
+        <LangPicker
+          style={styles.picker}
+          pickerItems={languages}
+          selectedLanguage={targetLanguage}
+          setSelectedLanguage={setTargetLanguage}
+        />
+      </View>
+      <TextBox
+        placeholder={CONFIG.INPUT_PLACEHOLDER}
+        textValue={text}
+        onChangeText={setText}
+        onSubmit={translate}
+      />
+      <Text style={styles.pickerDescription}>{CONFIG.OUTPUT_DESCRIPTION}</Text>
       <Text style={styles.translatedText}>{translatedText}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   pickerDescription: {
+    height: 50,
     fontSize: 15,
     fontWeight: 'bold',
     marginLeft: 5,
     paddingTop: 20,
   },
+  pickerContainer: {
+    justifyContent: 'space-between',
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   picker: {
-    borderColor: Colors.green,
+    width: '40%',
+  },
+  icon: {
+    height: 40,
+    width: 50,
   },
   translatedText: {
     marginLeft: 20,
